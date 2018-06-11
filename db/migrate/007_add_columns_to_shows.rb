e482cf450f78f9091d15fd36441816f1353698a4
Isaac Villicana <villicana.i@gmail.com>
2018-06-11 13:24:50 +0000

    automatically backed up by learn

--- db/migrate/007_add_columns_to_shows.rb
+++ db/migrate/007_add_columns_to_shows.rb
@@ -2,5 +2,7 @@ class AddColumnsToShows < ActiveRecord::Migration[4.2]
   def change
     add_column :shows, :name, :string
     add_column :shows, :genre, :string
+    add_column :shows, :day, :string
+    add_column :shows, :season, :string
   end
 end
